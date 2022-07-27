4ce3d1f84f5192a2be7ccfb5227efdc0351066c6
minh7721 <57635053+minh7721@users.noreply.github.com>
2022-07-27 17:09:54 +0700

    xong xã hội

---
+++ js/index.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        xanhLaDam: '#007882',
+      },
+    },
+    borderRadius: {
+        'btn' : '36px',
+        'none': '0',
+        'sm': '0.125rem',
+        DEFAULT: '0.25rem',
+        DEFAULT: '4px',
+        'md': '0.375rem',
+        'lg': '0.5rem',
+        'full': '9999px',
+        'large': '12px',
+      },
+  },
+};
