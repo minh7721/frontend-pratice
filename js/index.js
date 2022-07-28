cbf23765f43628d0deed4aebb87eabfdcbb1d954
minh7721 <57635053+minh7721@users.noreply.github.com>
2022-07-28 13:47:42 +0700

    responsive

--- js/index.js
+++ js/index.js
@@ -3,10 +3,11 @@ tailwind.config = {
     extend: {
       colors: {
         xanhLaDam: '#007882',
+        mauChu: '#3B4144',
       },
     },
     borderRadius: {
-        'btn' : '36px',
+        '36px' : '36px',
         'none': '0',
         'sm': '0.125rem',
         DEFAULT: '0.25rem',
@@ -16,5 +17,21 @@ tailwind.config = {
         'full': '9999px',
         'large': '12px',
       },
+      screens: {
+        'sm': '375px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      }
   },
 };
