8d3040361a8de027e0d4fcc6fc7c5d9888f83634
minh7721 <57635053+minh7721@users.noreply.github.com>
2022-07-29 08:07:39 +0700

    responsive

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ tailwind.config = {
       colors: {
         xanhLaDam: '#007882',
         mauChu: '#3B4144',
+        mauDo: '#D93C23',
       },
     },
     borderRadius: {
@@ -35,3 +36,36 @@ tailwind.config = {
       }
   },
 };
+
+
+const btn = document.querySelector("button.mobile-menu-button");
+const menu = document.querySelector(".mobile-menu");
+const btnCloseHeader = document.getElementById('btnCloseHeader');
+const text__reviewNha = document.getElementById('text__reviewNha');
+btn.addEventListener("click", () => {
+if (btn.style.display === "none") {
+btn.style.display = "block";
+btnCloseHeader.style.display = "none";
+text__reviewNha.style.display = "block";
+menu.classList.toggle("hidden");
+} else {
+btn.style.display = "none";
+btnCloseHeader.style.display = "block";
+text__reviewNha.style.display = "none";
+menu.classList.toggle("hidden");
+}
+});
+
+btnCloseHeader.addEventListener("click", () => {
+if (btnCloseHeader.style.display === "none") {
+btn.style.display = "none";
+btnCloseHeader.style.display = "block";
+text__reviewNha.style.display = "none";
+menu.classList.toggle("hidden");
+} else {
+btn.style.display = "block";
+btnCloseHeader.style.display = "none";
+text__reviewNha.style.display = "block";
+menu.classList.toggle("hidden");
+}
+});
